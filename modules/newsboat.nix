25bb105657594bd89818aca2db65886ed72dbbe8
Johan Hanses <johanhanses@gmail.com>
2025-01-06 15:56:06 +0100

    add os logos for starship prompt

--- modules/newsboat.nix
+++ modules/newsboat.nix
@@ -9,6 +9,9 @@
       {
         url = "https://nixos.org/blog/newsletters-rss.xml";
       }
+      {
+        url = "https://jnsgr.uk/posts/index.xml";
+      }
     ];
 
     extraConfig = "color info white black";
